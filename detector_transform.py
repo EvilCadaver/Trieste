305a5a0eba5b11fb1a81636d13ca1f4f728777f4
Denys Musiienko <68948002+EvilCadaver@users.noreply.github.com>
2026-07-27 17:01:48 +0200

    working on background

--- detector_transform.py
+++ detector_transform.py
@@ -5,16 +5,30 @@ from matplotlib.colors import LogNorm
 
 dataset_path = "/CCD/Image"
 
-scanname = ".\Data\FeRh_A04\Scan_061\\rawdata\Scan_061_326273873.h5"
-BGname = ".\Data\FeRh_A04\Scan_061_NoProbe\\rawdata\Scan_061_NoProbe_326262948.h5"
+fileScan = ".\Data\FeRh_A04\Scan_061\\rawdata\Scan_061_326273873.h5"
+fileDark = ".\Data\FeRh_A04\Scan_061_NoProbe\\rawdata\Scan_061_NoProbe_326262948.h5"
+fileNP = ".\Data\FeRh_A04\Scan_061_NoProbe\\rawdata\Scan_061_NoProbe_326262948.h5"
+fileOP = ".\Data\FeRh_A04\Scan_061_OnlyProbe\\rawdata\Scan_061_OnlyProbe_326264983.h5"
 
-with h5py.File(scanname, "r") as h5:
-    image_scan = h5[dataset_path][...]
+with h5py.File(fileScan, "r") as h5:
+    imageScan = h5[dataset_path][...]
 
-with h5py.File(BGname, "r") as h5:
-    image_BG = h5[dataset_path][...]
+with h5py.File(fileDark, "r") as h5:
+    imageDark = h5[dataset_path][...]
 
-image = image_scan - image_BG
+with h5py.File(fileNP, "r") as h5:
+    imageNP = h5[dataset_path][...]
+
+with h5py.File(fileOP, "r") as h5:
+    imageOP = h5[dataset_path][...]
+
+image = imageScan - imageDark - imageNP - imageOP
+
+areaBG = image[400:440,100:150]
+
+levelBG = np.median(image)
+
+image = image.astype(np.float64) - levelBG
 
 print(type(image))
 print(image.shape)
@@ -29,10 +43,10 @@ plt.imshow(
     image,
     origin="lower",
     cmap="gray",
-    norm=LogNorm(
-        vmin=max(1, np.nanpercentile(image, 1)),
-        vmax=np.nanpercentile(image, 99.9),
-    ),
+    # norm=LogNorm(
+    #     vmin=max(1, np.nanpercentile(image, 1)),
+    #     vmax=np.nanpercentile(image, 99.9),
+    # ),
 )
 
 plt.xlabel("Detector column")
