0d74957a958602d1c694d60f060e5d50506d845f
Denys Musiienko <68948002+EvilCadaver@users.noreply.github.com>
2026-08-05 15:07:15 +0200

    configs

--- analysisMultiFrame.py
+++ analysisMultiFrame.py
@@ -137,72 +137,40 @@ def readDelay(dataFilePath, h5DelayPath, delayZero):
     return round(float(-delayValue + delayZero), 1)
 
 ## Scan specific settings
-# Data folder location
-folderData = r"C:/git/Trieste/Data"
-# Sample name / subfolder in folderData
-sampleName = "FeRh_A04"
-# Scan specific names: 0) Scan name; 1) Data aqusition name; 2) Background 1 acqusition name; 3) Background 2 acquistion name; 4) Background 3 acqusition name
-scanNames = ["Scan", "", "NoProbe", "OnlyProbe", "Dark"]
-# Scan number chosen for analysis
-scanNo = 68
-# File size ratio for scan batches rejection
-minimumFileSizeRatio = 0.95
-# Verbose reporting on file scan results
-verbose = False
-
-## Single file analysis for now
-## HDF5 package paths
-h5CCDImagePath = "/CCD/Image"
-h5DelayPath = "/photon_source/SeedLaser/Delay_line_2"
-# Delay zero setting
-delayZero = -3096.49
-# Data indexes to exclude
-dataIndexesExcluded = [45, 46, 47, 48, 49]
-
-## Scan measurement parameters
-N_BINN = 2                      #Binning of the detector
-PIXEL_SIZE = N_BINN * 13.5e-6   #Pixel edge length, m
-LAMBDA = 23.5e-9                #Wavelength, m
-CY0 = 511                       #Reflection centre Y, pixels
-CX0 = 15                        #Reflection centre X, pixels
-DCCD = 67e-3                    #Shortest distance from the incident point on the sample to the detector, m 
-ALPHA = 17 /180*np.pi           #Incidence beam angle, rad
-OMEGA = 16.5 /180*np.pi         #Angle between scattered beam maximum and the detector normal (positive towards the sample surface), rad
-
-## Masks allignemnt
-# Make true for masks allignment
-alignMasks = False
-# Limit the region to show during masks allignment
-roiAllignMasks = np.s_[400:600, 0:220]
-
-# ROI for the background zero substraction
-roiBG = np.s_[400:450, 160:210] 
-
-## Masking regions, will be added to the empty mask, add rectangles as np.s_[y0:y1,x0:x1] to the list [ ] structure.
-maskBS = [np.s_[379:647,0:222], np.s_[511:1024,29:120]]
-
-## Q-space plot settings
-# Maximum number of reciprocal-space bins along the longer Qx/Qy dimension.
-Q_SPACE_BINS_MAX = 512
-
-## Q-space analysis
-# Show angles chosen for integration
-plotProfileAngles = True
-# Choose the highlight colour
-plotProfileAnglesColour = r"green"
-# Angle from Qx in deg
-ZETA = 0
-# Acceptance angle in deg
-D_ZETA = 180
-# Symmetry
-ZETA_SYMMETRY = 2
-# Radial step bin
-RADIAL_STEP_BIN = 1
-# Q-distance cutoffs in percent
-Q_LOW_CUTOFF = 15
-Q_HIGH_CUTOFF = 95
-# Polynomial background fitted only after applying both Q cutoffs
-BACKGROUND_NPOLY = 3
+from configs.FeRh_A06 import (
+    folderData,
+    sampleName,
+    scanNames,
+    scanNo,
+    minimumFileSizeRatio,
+    verbose,
+    h5CCDImagePath,
+    h5DelayPath,
+    delayZero,
+    dataIndexesExcluded,
+    N_BINN,
+    PIXEL_SIZE,
+    LAMBDA,
+    CY0,
+    CX0,
+    DCCD,
+    ALPHA,
+    OMEGA,
+    alignMasks,
+    roiAllignMasks,
+    roiBG,
+    maskBS,
+    Q_SPACE_BINS_MAX,
+    plotProfileAngles,
+    plotProfileAnglesColour,
+    ZETA,
+    D_ZETA,
+    ZETA_SYMMETRY,
+    RADIAL_STEP_BIN,
+    Q_LOW_CUTOFF,
+    Q_HIGH_CUTOFF,
+    BACKGROUND_NPOLY
+)
 
 ## Output files settings
 # Output folder
--- analysisSingleFrame.py
+++ analysisSingleFrame.py
@@ -5,67 +5,37 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 ## Scan specific settings
-# Data folder location
-folderData = r"C:/git/Trieste/Data"
-# Sample name / subfolder in folderData
-sampleName = "FeRh_A06"
-# Scan specific names: 0) Scan name; 1) Data aqusition name; 2) Background 1 acqusition name; 3) Background 2 acquistion name; 4) Background 3 acqusition name
-scanNames = ["Scan", "", "NoProbe", "OnlyProbe", "Dark"]
-# Scan number chosen for analysis
-scanNo = 27
-# File size ratio for scan batches rejection
-minimumFileSizeRatio = 0.95
-# Verbose reporting on file scan results
-verbose = False
-
-## Single file analysis for now
-## HDF5 package paths
-h5CCDImagePath = "/CCD/Image"
-h5DelayPath = "/photon_source/SeedLaser/Delay_line_2"
-# Delay zero setting
-delayZero = -3096.49
-# Single scan index to analyse
-dataIndex = 20
-
-## Scan measurement parameters
-N_BINN = 2                      #Binning of the detector
-PIXEL_SIZE = N_BINN * 13.5e-6   #Pixel edge length, m
-LAMBDA = 23.5e-9                #Wavelength, m
-CY0 = 498                       #Reflection centre Y, pixels
-CX0 = 50                        #Reflection centre X, pixels
-DCCD = 67e-3                    #Shortest distance from the incident point on the sample to the detector, m 
-ALPHA = 17 /180*np.pi           #Incidence beam angle, rad
-OMEGA = 16.5 /180*np.pi         #Angle between scattered beam maximum and the detector normal (positive towards the sample surface), rad
-
-## Masks allignemnt
-# Make true for masks allignment
-alignMasks = False
-# Limit the region to show during masks allignment
-roiAllignMasks = np.s_[345:610, 0:220]
-
-# ROI for the background zero substraction
-roiBG = np.s_[420:450, 60:90] 
-
-## Masking regions, will be added to the empty mask, add rectangles as np.s_[y0:y1,x0:x1] to the list [ ] structure.
-maskBS = [np.s_[342:612,0:222], np.s_[511:1024,24:118]]
-
-## Q-space plot settings
-# Maximum number of reciprocal-space bins along the longer Qx/Qy dimension.
-Q_SPACE_BINS_MAX = 512
-
-## Q-space analysis
-# Show angles chosen for integration
-plotProfileAngles = True
-# Choose the highlight colour
-plotProfileAnglesColour = r"green"
-# Angle from Qx in deg
-ZETA = 45
-# Acceptance angle in deg
-D_ZETA = 20
-# Symmetry
-ZETA_SYMMETRY = 4
-# Radial step bin
-RADIAL_STEP_BIN = 2
+from configs.FeRh_A06 import (
+    folderData,
+    sampleName,
+    scanNames,
+    scanNo,
+    minimumFileSizeRatio,
+    verbose,
+    h5CCDImagePath,
+    h5DelayPath,
+    delayZero,
+    dataIndex,
+    N_BINN,
+    PIXEL_SIZE,
+    LAMBDA,
+    CY0,
+    CX0,
+    DCCD,
+    ALPHA,
+    OMEGA,
+    alignMasks,
+    roiAllignMasks,
+    roiBG,
+    maskBS,
+    Q_SPACE_BINS_MAX,
+    plotProfileAngles,
+    plotProfileAnglesColour,
+    ZETA,
+    D_ZETA,
+    ZETA_SYMMETRY,
+    RADIAL_STEP_BIN,
+)
 
 ## Calling the folder scanning function
 results = scanFiles(
